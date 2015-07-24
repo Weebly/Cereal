4b2ea4326ced3b1475421aa054c5ae3bd6b7df3b
ketzusaka <james@weebly.com>
2015-07-24 08:15:53 -0700

    Initial Commit

---
+++ Cereal/Cereal.h
@@ -0,0 +1,19 @@
+//
+//  Cereal.h
+//  Cereal
+//
+//  Created by James Richard on 7/24/15.
+//  Copyright © 2015 Weebly. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Cereal.
+FOUNDATION_EXPORT double CerealVersionNumber;
+
+//! Project version string for Cereal.
+FOUNDATION_EXPORT const unsigned char CerealVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Cereal/PublicHeader.h>
+
+
