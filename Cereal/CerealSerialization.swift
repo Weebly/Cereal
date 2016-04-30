fc9aec331ac85be5add72b6cfa12a48b05299aef
Сергей Галездинов <segazero@gmail.com>
2016-04-30 19:44:36 +0300

    decoding CoderTreeValue from raw bytes

--- Cereal/CerealSerialization.swift
+++ Cereal/CerealSerialization.swift
@@ -68,6 +68,12 @@ private func fromByteArray<T>(value: [UInt8], _: T.Type) -> T {
     }
 }
 
+private func countValue<T>(from: [UInt8]) -> T {
+    return from.withUnsafeBufferPointer({
+        UnsafePointer<T>($0.baseAddress).memory
+    })
+}
+
 private func stringEntriesCount(array: [CoderTreeValue]) -> Int {
     var result = 0
     for item in array {
@@ -206,10 +212,138 @@ private extension CoderTreeValue {
     }
 }
 
+// MARK: decoding extensions
+
+private extension CoderTreeValue {
+    static func readInt(inout bytes: [UInt8], inout offset: Int) -> Int? {
+        guard bytes.count > offset + sizeof(Int) else { return nil }
+
+        let bytesForCount: [UInt8] = Array(bytes[offset..<offset + sizeof(Int)])
+        let value: Int = countValue(bytesForCount)
+
+        offset += sizeof(Int)
+
+        return value
+    }
+
+    static func readArray(inout bytes: [UInt8], inout offset: Int, capacity: Int, endOffset: Int) -> [CoderTreeValue]? {
+        var array = [CoderTreeValue]()
+        array.reserveCapacity(capacity)
+        while offset < endOffset {
+            guard let value = CoderTreeValue(bytes: &bytes, offset: &offset) else { return nil }
+            array.append(value)
+        }
+
+        return array
+    }
+
+    init?(inout bytes: [UInt8], inout offset: Int) {
+        guard bytes.count > offset + 1 else { return nil }
+        guard let type = CerealCoderTreeValueType(rawValue: bytes[offset]) else { return nil }
+
+        offset += 1
+
+        if type == .Pair {
+            guard let key = CoderTreeValue(bytes: &bytes, offset: &offset),
+                let value = CoderTreeValue(bytes: &bytes, offset: &offset) else { return nil }
+            self = .PairValue(key, value)
+
+            return
+        }
+
+        guard let count = CoderTreeValue.readInt(&bytes, offset: &offset) else { return nil }
+
+        guard bytes.count >= offset + count else { return nil }
+
+        let capacity: Int
+
+        if [CerealCoderTreeValueType.Array, CerealCoderTreeValueType.SubTree].contains(type) {
+            guard let capacityValue = CoderTreeValue.readInt(&bytes, offset: &offset) else { return nil }
+            capacity = capacityValue
+        } else {
+            capacity = 0
+        }
+
+        let startIndex = offset
+        let endIndex = offset + count
+
+        guard bytes.count >= endIndex else { return nil }
+
+        switch type {
+            case .String, .NSURL:
+                let valueBytes = Array(bytes[startIndex..<endIndex])
+
+                guard let string = String(bytes: valueBytes, encoding:  NSUTF8StringEncoding) else { return nil }
+                if type == .String {
+                    self = .StringValue(string)
+                } else {
+                    guard let url = NSURL(string: string) else { return nil }
+                    self = .NSURLValue(url)
+                }
+
+            case .Int:
+                let valueBytes = Array(bytes[startIndex..<endIndex])
+                let value = fromByteArray(valueBytes, Int.self)
+                self = .IntValue(value)
+
+            case .Int64:
+                let valueBytes = Array(bytes[startIndex..<endIndex])
+                let value = fromByteArray(valueBytes, Int64.self)
+                self = .Int64Value(value)
+
+            case .Bool:
+                let valueBytes = Array(bytes[startIndex..<endIndex])
+                let value = valueBytes[0]
+                self = .BoolValue(value == 1)
+
+            case .Float:
+                let valueBytes = Array(bytes[startIndex..<endIndex])
+                let value = fromByteArray(valueBytes, Float.self)
+                self = .FloatValue(value)
+
+            case .Double, .NSDate:
+                let valueBytes = Array(bytes[startIndex..<endIndex])
+                let value = fromByteArray(valueBytes, Double.self)
+                if type == .Double {
+                    self = .DoubleValue(value)
+                } else {
+                    let date = NSDate(timeIntervalSinceReferenceDate: value)
+                    self = .NSDateValue(date)
+                }
+
+            case .Array, .SubTree:
+                guard let array = CoderTreeValue.readArray(&bytes, offset: &offset, capacity: capacity, endOffset: endIndex) else { return nil }
+
+                if type == .Array {
+                    self = .ArrayValue(array)
+                } else {
+                    self = .SubTree(array)
+                }
+
+            case .IdentifyingTree:
+                let valueBytes = Array(bytes[startIndex..<endIndex])
+
+                guard let identifier = String(bytes: valueBytes, encoding:  NSUTF8StringEncoding) else { return nil }
+                offset = endIndex
+
+                guard let array = CoderTreeValue.readArray(&bytes, offset: &offset, capacity: capacity, endOffset: endIndex) else { return nil }
+
+                self = .IdentifyingTree(identifier, array)
+                return
+
+            default:
+                return nil
+        }
+
+        offset = endIndex
+    }
+}
+
 extension CoderTreeValue: CerealByteConvertible {
-    init?(bytes: [UInt8], startOffset: Int) {
-        //TODO: implement
-        return nil
+    init?(bytes: [UInt8]) {
+        var bytes = bytes
+        var offset = 0
+        self.init(bytes: &bytes, offset: &offset)
     }
     func bytes() -> [UInt8] {
         var result = [UInt8]()
